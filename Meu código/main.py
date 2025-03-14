9bb89afe1ad75eb0823ece78ed081a88412a9308
JonatasPCS05 <jonataspinheiro05@gmail.com>
2025-03-13 22:58:58 -0300

    13/03/2025

--- Meu código/main.py
+++ Meu código/main.py
@@ -1,1 +1,12 @@
-print('Hello World!')
+def logar(email, senha):
+    if email in emails:
+        if emails[email] == senha:
+            print("Loguin efetuado com sucesso")
+        else:
+            print("Senha incorreta")
+    else:
+        print("Email não encontrado")
+
+emails = {"jonatas@gmail.com": 1234}
+
+logar('jonatas@gmail.com', 1234)
